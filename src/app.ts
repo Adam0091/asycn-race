083f1526fcd4721c437d0491281351dd33a72a66
Andrey Chernobrovkin <adam.holo@mail.ru>
2021-06-12 07:20:51 +0400

    feat: delete tableWinners and tableMachines and rename machines

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import { Header } from "./components/header/header";
-import { ControlPanel } from "./components/controlPanel/controlPanel"
+import { ControlPanel } from "./components/controlPanel/controlPanel";
+import { Garage } from "./components/garage/garage";
+import "./style/main.scss";
 
 const ROOT = document.createElement("div");
 ROOT.classList.add(".root");
 document.body.appendChild(ROOT);
 
 const HEADER = new Header();
-const CONTROL_PANEL = new ControlPanel()
+const CONTROL_PANEL = new ControlPanel();
+const GARAGE = new Garage();
 
 ROOT.appendChild( HEADER.init() );
 ROOT.appendChild( CONTROL_PANEL.init() );
+ROOT.appendChild( GARAGE.init() );

8b5be50b20aad34347e309742f86606f0ae279a4
Andrey Chernobrovkin <adam.holo@mail.ru>
2021-06-12 07:17:11 +0400

    feat: add not full controlPanel

--- src/components/controlPanel/controlPanel.ts
+++ src/components/controlPanel/controlPanel.ts
@@ -14,15 +14,16 @@ export class ControlPanel {
         return this.control_panel;
     }
     private drawCreateCarInput():void {
-        const create_car_input = createAndAppendHtmlElement( this.control_panel, "div", "create_car_input" );
+        const create_car_input = createAndAppendHtmlElement( this.control_panel, "div", "control_panel__create_car_input" );
         const create_car_input__car_name = createAndAppendHtmlElement( create_car_input, "input", "create_car_input__car_name" );
         create_car_input__car_name.setAttribute( "type", "text" );
         const create_car_input__car_color = createAndAppendHtmlElement( create_car_input, "input", "create_car_input__car_color" );
         create_car_input__car_color.setAttribute( "type", "color" );
         const create_car_input__btn = createAndAppendHtmlElement( create_car_input, "button", "create_car_input__btn", "CREATE" );
+        create_car_input__btn.classList.add("button");
     }
     private drawUpdateCarInput():void {
-        const update_car_input = createAndAppendHtmlElement( this.control_panel, "div", "update_car_input" );
+        const update_car_input = createAndAppendHtmlElement( this.control_panel, "div", "control_panel__update_car_input" );
         const update_car_input__car_name = createAndAppendHtmlElement( update_car_input, "input", "update_car_input__car_name" );
         update_car_input__car_name.setAttribute( "type", "text" );
         const update_car_input__car_color = createAndAppendHtmlElement( update_car_input, "input", "update_car_input__car_color" );
@@ -34,5 +35,8 @@ export class ControlPanel {
         const control_panel__btn_race = createAndAppendHtmlElement( control_panel__buttons, "button", "control_panel__btn_race", "RACE" );
         const control_panel__btn_reset = createAndAppendHtmlElement( control_panel__buttons, "button", "control_panel__btn_reset", "RESET" );
         const control_panel__btn_generate_cars = createAndAppendHtmlElement( control_panel__buttons, "button", "control_panel__btn_generate_cars", "GENERATE CARS" );
+        control_panel__btn_race.classList.add("button");
+        control_panel__btn_reset.classList.add("button");
+        control_panel__btn_generate_cars.classList.add("button");
     }
 }

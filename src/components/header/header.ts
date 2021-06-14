a73d305590805c5d2d787029a4ad6f8c66b71712
Andrey(laptop) <adam.holo@mail.ru>
2021-06-14 13:44:18 +0400

    feat: add header scss and completed header.ts

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,7 +1,15 @@
 import { createAndAppendHtmlElement } from "../../typescript/add-element-function";
 
+
+interface Header_BTNS {
+    garage_btn?:HTMLElement,
+    winners_btn?:HTMLElement,
+}
+
 export class Header {
     private header:HTMLElement;
+    private BUTTONS:Header_BTNS;
+
 
     public init():HTMLElement {
         this.header = document.createElement("header");
@@ -12,9 +20,25 @@ export class Header {
     }
     private drawNav():void {
         const header__nav = createAndAppendHtmlElement( this.header, "nav", "header__nav" );
+
         const nav__garage_btn = createAndAppendHtmlElement( header__nav, "button", "nav__garage_btn", "TO GARAGE" );
         nav__garage_btn.classList.add("button");
+
         const nav__winners_btn = createAndAppendHtmlElement( header__nav, "button", "nav__winners_btn", "TO WINNERS" );
         nav__winners_btn.classList.add("button");
+
+        this.BUTTONS = {
+            garage_btn: nav__garage_btn,
+        }
+        this.BUTTONS.winners_btn = nav__winners_btn;
+    }
+    
+    public get layout() : HTMLElement {
+        return this.header;
+    }
+    
+    
+    public get buttons() : Header_BTNS {
+        return this.BUTTONS;
     }
 }

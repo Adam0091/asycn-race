320db52b9b6531412ca5dab845ab10249f87d3d9
Andrey(laptop) <adam.holo@mail.ru>
2021-06-14 13:45:55 +0400

    feat: add garage scss and completed garage.ts

--- src/components/garage/garage.ts
+++ src/components/garage/garage.ts
@@ -8,29 +8,59 @@ export class Garage {
     private garage:HTMLElement;
     private number_page:number = 1;
     private garage__title:HTMLElement;
-
+    private ok:boolean = false;
+    private arr_machine:Array<Machine> = [];
+    private garage__list: HTMLElement;
+    
     public init():HTMLElement {
         this.garage = document.createElement("div");
         this.garage.classList.add("garage")
-
         this.drawTitle();
-        this.drawListMachines()
-
+        this.getCarsServer().then((resolve) => {
+            this.ok = true;
+            this.drawListMachines(resolve);
+        });
         return this.garage;
+    }
+
+    private async getCarsServer() {
+        const cars = await sendRequest('GET', ConstURL.garageURL);
+        return await Promise.resolve(cars);
     }
     private drawTitle():void {
         this.garage__title = createAndAppendHtmlElement(this.garage, "h2", "garage__title", `${ ConstText.garage__title } (${ 0 }) ` );
         createAndAppendHtmlElement(this.garage, "h3", "garage__page", `${ ConstText.garage__page } (${ this.number_page }) ` );
     }
-    private async drawListMachines() {
-        const garage__list = createAndAppendHtmlElement(this.garage, "div", "garage__list");
-        const cars = await sendRequest('GET', ConstURL.garageURL);
+    private drawListMachines(cars:Array<{id:number,name:string,color:string}>) {
+        this.garage__list = createAndAppendHtmlElement(this.garage, "div", "garage__list");
 
-        this.garage__title.innerText = `${ ConstText.garage__title } (${ cars.length }) `
+        document.querySelector(".garage__title").innerHTML = `${ ConstText.garage__title } (${ cars.length }) `;
         for ( let i = 0; i < cars.length; i++ ) {
-            const garage__item = createAndAppendHtmlElement(garage__list, "div", "garage__item");
-            const machine = new Machine(cars[i].id, cars[i].name, cars[i].color);
-            garage__item.appendChild( machine.init() );
+            const garage__item = createAndAppendHtmlElement(this.garage__list, "div", "garage__item");
+            const MACHINE = new Machine(cars[i].id, cars[i].name, cars[i].color);
+            garage__item.appendChild( MACHINE.init() );
+            this.arr_machine.push( MACHINE );
         }
     }
+
+    public update() {
+        this.ok = false;
+        setTimeout(()=>{
+            this.getCarsServer().then((cars) => {
+                this.drawListMachines(cars);
+    
+                const garage__list = document.querySelector(".garage__list");
+                garage__list.innerHTML = "";
+                garage__list.appendChild(this.garage__list)
+                this.ok = true;
+            });
+        },100)
+    }
+    public get OK() : boolean {
+        return this.ok;
+    }
+    public get arrMachine() : Array<Machine> {
+        return this.arr_machine;
+    }
+    
 }

9540a932eacfd4cbb875a121dd406333de399b57
Andrey Chernobrovkin <adam.holo@mail.ru>
2021-06-15 13:33:05 +0400

    feat: removed unnecessary variables added functions to update the title of the garage and the list of cars in the garage, added buttons to switch between pages and added public methods to the Garage class to change the page number

--- src/components/const/const.ts
+++ src/components/const/const.ts
@@ -3,3 +3,6 @@ export const garage__page = "Page";
 
 export const machine_btn_select = "SELECT";
 export const machine_btn_remove = "REMOVE";
+
+export const btn_back = "BACK";
+export const btn_next = "NEXT";
--- src/components/garage/garage.ts
+++ src/components/garage/garage.ts
@@ -7,60 +7,99 @@ import * as ConstText from "../const/const";
 export class Garage {
     private garage:HTMLElement;
     private number_page:number = 1;
-    private garage__title:HTMLElement;
-    private ok:boolean = false;
-    private arr_machine:Array<Machine> = [];
-    private garage__list: HTMLElement;
+    private coutPage: number = 1;
     
     public init():HTMLElement {
         this.garage = document.createElement("div");
         this.garage.classList.add("garage")
         this.drawTitle();
         this.getCarsServer().then((resolve) => {
-            this.ok = true;
             this.drawListMachines(resolve);
+            this.drawButtonsForPages()
         });
         return this.garage;
     }
-
+    private drawTitle():void {
+        createAndAppendHtmlElement(this.garage, "h2", "garage__title", `${ ConstText.garage__title } (${ 0 }) ` );
+        createAndAppendHtmlElement(this.garage, "h3", "garage__page", `${ ConstText.garage__page } (${ this.number_page }) ` );
+    }
     private async getCarsServer() {
         const cars = await sendRequest('GET', ConstURL.garageURL);
         return await Promise.resolve(cars);
     }
-    private drawTitle():void {
-        this.garage__title = createAndAppendHtmlElement(this.garage, "h2", "garage__title", `${ ConstText.garage__title } (${ 0 }) ` );
-        createAndAppendHtmlElement(this.garage, "h3", "garage__page", `${ ConstText.garage__page } (${ this.number_page }) ` );
-    }
-    private drawListMachines(cars:Array<{id:number,name:string,color:string}>) {
-        this.garage__list = createAndAppendHtmlElement(this.garage, "div", "garage__list");
+    private drawListMachines(cars:Array<{id:number,name:string,color:string}>):void {
+        this.coutPage = Math.ceil( cars.length / 7 );
 
+        const garage__list = createAndAppendHtmlElement(this.garage, "div", "garage__list");
         document.querySelector(".garage__title").innerHTML = `${ ConstText.garage__title } (${ cars.length }) `;
-        for ( let i = 0; i < cars.length; i++ ) {
-            const garage__item = createAndAppendHtmlElement(this.garage__list, "div", "garage__item");
+
+        const start = this.number_page === 1 ? 0 :  this.number_page * 7 - 1;
+        const end = this.number_page * 7 >  cars.length ? cars.length : this.number_page * 7;
+
+        for ( let i = start; i < end; i++ ) {
+            const garage__item = createAndAppendHtmlElement(garage__list, "div", "garage__item");
             const MACHINE = new Machine(cars[i].id, cars[i].name, cars[i].color);
             garage__item.appendChild( MACHINE.init() );
-            this.arr_machine.push( MACHINE );
         }
     }
+    private drawButtonsForPages():void {
+        const garage__buttons_for_pages = createAndAppendHtmlElement(this.garage, "div", "garage__buttons_for_pages");
 
-    public update() {
-        this.ok = false;
-        setTimeout(()=>{
-            this.getCarsServer().then((cars) => {
-                this.drawListMachines(cars);
-    
-                const garage__list = document.querySelector(".garage__list");
-                garage__list.innerHTML = "";
-                garage__list.appendChild(this.garage__list)
-                this.ok = true;
-            });
-        },100)
+        const buttons_for_pages__back = createAndAppendHtmlElement(garage__buttons_for_pages, "button", "buttons_for_pages__back", ConstText.btn_back) as HTMLButtonElement;
+        buttons_for_pages__back.classList.add("button");
+        if( this.number_page >  1)  buttons_for_pages__back.disabled = false;
+        else buttons_for_pages__back.disabled = true;
+        
+        const buttons_for_pages__next = createAndAppendHtmlElement(garage__buttons_for_pages, "button", "buttons_for_pages__next", ConstText.btn_next) as HTMLButtonElement;
+        buttons_for_pages__next.classList.add("button");
+        if( this.number_page < this.coutPage)  buttons_for_pages__next.disabled = false;
+        else buttons_for_pages__next.disabled = true;
     }
-    public get OK() : boolean {
-        return this.ok;
+    public update():void {
+        this.getCarsServer().then((cars) => {
+            this.updateTitle(cars);
+            this.updateGarageList(cars);
+            this.updateButtonsForPages();
+        });
     }
-    public get arrMachine() : Array<Machine> {
-        return this.arr_machine;
+    private updateTitle(cars:Array<{id:number,name:string,color:string}>):void {
+        document.querySelector(".garage__title").innerHTML = `${ ConstText.garage__title } (${ cars.length }) `;
+        document.querySelector(".garage__page").innerHTML = `${ ConstText.garage__page } (${ this.number_page }) `;
     }
-    
+    private updateGarageList(cars:Array<{id:number,name:string,color:string}>):void {
+        const garage__list = document.querySelector(".garage__list") as HTMLElement;
+        garage__list.innerHTML = "";
+        
+        this.coutPage = Math.ceil( cars.length / 7 );
+        
+        const start = this.number_page === 1 ? 0 : ( (this.number_page - 1) * 7);
+        const end = this.number_page * 7 >  cars.length ? cars.length : this.number_page * 7;
+
+        for ( let i = start; i < end; i++ ) {
+            const garage__item = createAndAppendHtmlElement(garage__list, "div", "garage__item");
+            const MACHINE = new Machine(cars[i].id, cars[i].name, cars[i].color);
+            garage__item.appendChild( MACHINE.init() );
+        }
+
+    }
+    private updateButtonsForPages():void {
+        const buttons_for_pages__back = document.querySelector(".buttons_for_pages__back") as HTMLButtonElement;
+        const buttons_for_pages__next = document.querySelector(".buttons_for_pages__next") as HTMLButtonElement;
+
+        if( this.number_page >  1)  buttons_for_pages__back.disabled = false;
+        else buttons_for_pages__back.disabled = true;
+
+        if( this.number_page < this.coutPage)  buttons_for_pages__next.disabled = false;
+        else buttons_for_pages__next.disabled = true;
+    }
+
+    public get layout():HTMLElement {
+        return this.garage;
+    }
+    public nextPage():void {
+        this.number_page++;
+    }
+    public backPage():void {
+        this.number_page--;
+    }
 }

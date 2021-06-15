a197586902302c04d1a6b9b722a2b7be79073b58
Andrey Chernobrovkin <adam.holo@mail.ru>
2021-06-15 13:56:42 +0400

    refactoring: creating beauty in the header class

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -8,7 +8,7 @@ interface Header_BTNS {
 
 export class Header {
     private header:HTMLElement;
-    private BUTTONS:Header_BTNS;
+    private BUTTONS:Header_BTNS = {};
 
 
     public init():HTMLElement {
@@ -21,23 +21,20 @@ export class Header {
     private drawNav():void {
         const header__nav = createAndAppendHtmlElement( this.header, "nav", "header__nav" );
 
-        const nav__garage_btn = createAndAppendHtmlElement( header__nav, "button", "nav__garage_btn", "TO GARAGE" );
+        const nav__garage_btn = createAndAppendHtmlElement( header__nav, "button", "nav__garage_btn", "TO GARAGE" ) as HTMLButtonElement;
+        nav__garage_btn.disabled = true;
         nav__garage_btn.classList.add("button");
 
         const nav__winners_btn = createAndAppendHtmlElement( header__nav, "button", "nav__winners_btn", "TO WINNERS" );
         nav__winners_btn.classList.add("button");
 
-        this.BUTTONS = {
-            garage_btn: nav__garage_btn,
-        }
+        this.BUTTONS.garage_btn =  nav__garage_btn;
         this.BUTTONS.winners_btn = nav__winners_btn;
     }
     
     public get layout() : HTMLElement {
         return this.header;
     }
-    
-    
     public get buttons() : Header_BTNS {
         return this.BUTTONS;
     }

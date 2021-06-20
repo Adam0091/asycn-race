0cca9c2d0ad58115329699988f84ce4d54bed6fb
Andrey Chernobrovkin <adam.holo@mail.ru>
2021-06-21 03:07:43 +0400

    feat: added arrays of brands and brands of cars

---
+++ src/components/const/brands_and_models_of_cars.ts
@@ -0,0 +1,25 @@
+export const brands = [
+    "Lada",
+    "Toyota",
+    "Nissan",
+    "Hyundai",
+    "Kia",
+    "Renault",
+    "Chevrolet",
+    "Volkswagen",
+    "Ford",
+    "Mitsubishi"
+];
+interface CAR_MODELS { [key: string]: string[] }
+export const mordels: CAR_MODELS = {
+    "Lada": [ "Niva Legend", "Granta", "Largus", "Vesta", "XRAY", "Niva Travel", "Priora", "Kalina 2", "110", "Samara"],
+    "Toyota": [ "4Runner", "2000GT", "Agya", "Allion", "Alphard", "Aqua", "Aurion", "Auris", "Avalon", "Avensis" ],
+    "Nissan": [ "180SX", "300ZX", "350Z", "370Z", "Almera", "Altima Hybrid", "Armada", "Avenir", "Bluebird", "Bluebird Sylphy" ],
+    "Hyundai": [ "Accent", "Atos", "Chorus", "County", "Creta", "Dynasty", "Elantra", "Entourage", "Eon", "Equus" ],
+    "Kia": [ "Bongo", "Cadenza", "Carens", "Carnival", "Cerato", "Niro", "Magentis", "Mohave", "Opirus", "Pegas" ],
+    "Renault": [ "3", "4", "5", "7", "Fluence", "Magnum", "Master", "Safrane", "Monastella", "Sherpa 5" ],
+    "Chevrolet": [ "150", "Apache", "Avalanche", "Aveo", "Bel Air", "Biscayne", "Camaro", "Cobalt", "G506", "Chevelle" ],
+    "Volkswagen": [ "Amarok", "Beetle (A5)", "Caddy", "Crafter", "Fox", "Gol", "Golf", "Golf I", "Golf IV", "Golf Plus" ],
+    "Ford": [ "Aerostar", "Airstream", "Anglia", "B-Max", "Bantam", "C-Max", "C1", "EcoSport", "Edge", "Escape Hybrid" ],
+    "Mitsubishi": [ "ASX", "Carisma", "Delica", "Diamante", "Dingo", "Eclipse", "Jeep", "L200", "Lancer", "Lancer Evolution" ]
+}

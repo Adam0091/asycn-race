7692e61a22972c820c57a5ed655fa65f7532be56
Andrey Chernobrovkin <adam.holo@mail.ru>
2021-06-21 03:12:24 +0400

    feat: added a feature to send the winner

--- src/components/machine/machineEvent.ts
+++ src/components/machine/machineEvent.ts
@@ -12,8 +12,24 @@ export async function stopEngine(id:number) {
     const cars = await sendRequest('GET', ConstURL.engineURL+`?id=${id}&status=stopped`);
     return await Promise.resolve(cars);
 }
-
 export async function carSuccess(id:number) {
     const success = await sendRequest('GET', ConstURL.engineURL+`?id=${id}&status=drive`);
     return await Promise.resolve(success);
+}
+export async function sendWinner(id:number, time: number) {
+    const itIsWinners = sendRequest('GET', ConstURL.winnersURL+`/${id}`);
+    itIsWinners.then( (winner) => {
+        if(Object.keys(winner).length !== 0) {
+            sendRequest( 'PUT', ConstURL.winnersURL+`/${id}`, JSON.stringify({
+                wins: ++winner.wins,
+                time: time 
+            }) )
+        } else {
+            sendRequest('POST', ConstURL.winnersURL, JSON.stringify({
+                id: id,
+                wins: 1,
+                time: time
+            }));
+        }
+    });
 }

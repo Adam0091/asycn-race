d945c22b04c5b6b20f84ec32db34965bf046b9cf
Andrey Chernobrovkin <adam.holo@mail.ru>
2021-06-21 03:09:31 +0400

    feat: added implementation of generating 100 machines

--- src/components/controlPanel/controlPanelEvent.ts
+++ src/components/controlPanel/controlPanelEvent.ts
@@ -1,5 +1,6 @@
 import { sendRequest } from "../../typescript/sendRequest";
 import * as ConstURL from "../const/constURL";
+import * as BrandsAndModel from "../const/brands_and_models_of_cars";
 
 export function createCar(name:string, color:string) {
     sendRequest ('POST', ConstURL.garageURL, JSON.stringify({
@@ -38,5 +39,19 @@ export function reset() {
     })
 }
 export function generateCars() {
-    console.log("generate cars");
+    for(let i = 0; i < 100; i++) {
+        const randomBrands = Math.floor( Math.random() * 10 );
+        const brand = String(BrandsAndModel.brands[randomBrands])
+        const randomModel = Math.floor( Math.random() * 10 );
+        const model = BrandsAndModel.mordels[brand][randomModel];
+        const name = `${brand}  ${model}`;
+
+        let color = "#";
+        for(let j = 0; j < 6; j++) {
+            const n = Math.floor( Math.random() * 16 );
+            color += Number(n).toString(16)
+        }
+
+        createCar(name, color);
+    }
 }
